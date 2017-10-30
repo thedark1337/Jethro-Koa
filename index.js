2ad4f6fbad844215dbdfd1235359f26d3c1548ae
Thedark1337 <thedark1337@thedark1337.com>
2017-10-29 20:18:40 -0500

    Breaking: Change syntax to Koa v2 syntax

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 const chalk = require('chalk');
 const Counter = require('passthrough-counter');
-const humanize = require('humanize');
+const humanFormat = require('human-format');
 const logger = require('jethro');
-const prettyMs = require('pretty-ms');
-const util = require('util');
 
 logger.set({
     quickStart: true,
@@ -13,87 +11,91 @@ logger.set({
 });
 
 function time(start) {
-    return prettyMs(new Date() - start);
+    return humanFormat(new Date() - start, {
+        seperator: '',
+        unit: 'ms'
+    });
 }
 
-function mainLog() {
-    /** @this KoaJS
-     *   @param {object} next KoaJS Ctx Object.
-     */
-    return function *log(next) {
-        // request
+function jethroLogger() {
+    return function log(ctx, next) {
         const start = new Date();
-        const IP = this.headers['x-forwarded-for'] || this.headers['X-Real-IP'] || this.ip || '0.0.0.0';
-        try {
-            yield next;
-        } catch (err) {
-            logger('error', 'KoaJS', util.format('%s %s %s %s %s', IP, this.method, this.originalUrl, this.status, time(start), err.message));
-            throw err;
-        }
+        const IP = ctx.headers['x-forwarded-for'] || ctx.headers['X-Real-IP'] || ctx.ip || '0.0.0.0';
 
-        // calculate the length of a streaming response by intercepting the stream with a counter. Only necessary if a content-length header is currently not set.
-        const length = this.responseLength;
-        const body = this.body;
-        let counter;
-        if (length == null && body && body.readable) {
-            this.body = body
-                .pipe(counter = Counter())
-                .on('error', this.onerror);
-        }
+        return next().then(() => {
 
-        const ctx = this; //eslint-disable-line
-        const res = this.res;
+            // calculate the length of a streaming response by intercepting the stream with a counter. Only necessary if a content-length header is currently not set.
+            const length = ctx.response.length;
+            const body = ctx.body;
+            let counter;
 
-        function done() {
-            /*eslint-disable no-use-before-define */
-            res.removeListener('finish', onfinish);
-            res.removeListener('close', onclose);
-            /*eslint-enable no-use-before-define */
-            const len = (counter ? counter.length : length);
-            let method;
-            let responseLength;
-            let status;
-            let level;
-            if (ctx.method === 'POST' || ctx.method === 'PUT') {
-                level = 'info';
-                method = chalk.yellow(ctx.method);
-            } else if (ctx.method === 'DELETE') {
-                level = 'warning';
-                method = chalk.red(ctx.method);
-            } else if (ctx.method === 'GET') {
-                level = 'info';
-                method = chalk.green(ctx.method);
-            } else {
-                level = 'info';
-                method = chalk.green(ctx.method);
+            if (length == null && body && body.readable) {
+                ctx.body = body
+                    .pipe(counter = new Counter())
+                    .on('error', ctx.onerror);
             }
-            if (ctx.status >= 500) {
-                level = 'error';
-                status = chalk.red(ctx.status);
-            } else if (ctx.status >= 400) {
-                level = 'warning';
-                status = chalk.yellow(ctx.status);
-            } else if (ctx.status >= 100) {
-                level = 'info';
-                status = chalk.green(ctx.status);
+            const res = ctx.res;
+
+            function done() {
+
+                /* eslint-disable no-use-before-define */
+                res.removeListener('finish', onfinish);
+                res.removeListener('close', onclose);
+
+                /* eslint-enable no-use-before-define */
+                const len = (counter ? counter.length : length);
+                let method;
+                let responseLength;
+                let status;
+                let level;
+
+                if (ctx.method === 'POST' || ctx.method === 'PUT') {
+                    level = 'info';
+                    method = chalk.yellow(ctx.method);
+                } else if (ctx.method === 'DELETE') {
+                    level = 'warning';
+                    method = chalk.red(ctx.method);
+                } else if (ctx.method === 'GET') {
+                    level = 'info';
+                    method = chalk.green(ctx.method);
+                } else {
+                    level = 'info';
+                    method = chalk.green(ctx.method);
+                }
+                if (ctx.status >= 500) {
+                    level = 'error';
+                    status = chalk.red(ctx.status);
+                } else if (ctx.status >= 400) {
+                    level = 'warning';
+                    status = chalk.yellow(ctx.status);
+                } else if (ctx.status >= 100) {
+                    level = 'info';
+                    status = chalk.green(ctx.status);
+                }
+
+                if ([204, 205, 304].indexOf(ctx.status) > -1) {
+                    responseLength = '';
+                } else if (len == null) {
+                    responseLength = '-';
+                } else {
+                    responseLength = humanFormat(len, {
+                        seperator: '',
+                        unit: 'B'
+                    });
+                }
+                logger(level, 'KoaJS', `${IP} ${status} ${method} ${ctx.originalUrl} ${time(start)} ${responseLength}`);
             }
 
-            if ([204, 205, 304].indexOf(ctx.status) > -1) {
-                responseLength = '';
-            } else if (len == null) {
-                responseLength = '-';
-            } else {
-                responseLength = humanize.filesize(len);
-            }
-            logger(level, 'KoaJS', util.format('%s %s %s %s %s', IP, status, method, ctx.originalUrl, time(start), responseLength));
-        }
+            // Logs when the response is finished or closed, whichever happens first.
+            const onfinish = done.bind(null, 'finish');
+            const onclose = done.bind(null, 'close');
 
-        // Logs when the response is finished or closed, whichever happens first.
-        const onfinish = done.bind(null, 'finish');
-        const onclose = done.bind(null, 'close');
-        res.once('finish', onfinish);
-        res.once('close', onclose);
+            res.once('finish', onfinish);
+            res.once('close', onclose);
+        })
+            .catch((err) => {
+                logger('error', 'KoaJS', `${IP} ${ctx.method} ${ctx.originalUrl} ${ctx.status} ${time(start)} ${process.env.NODE_ENV === 'production' ? err.message : err.stack}`);
+            });
     };
 }
-
-module.exports = mainLog;
+module.exports = jethroLogger;
